a549665e56ff6897e0cae054e670b64f42025af1
MadhuriKadam45 <Madhurikadam45@proton.me>
2024-10-27 08:31:59 +0000

    data send & received file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-console.log("Connected");
-
 /* API Endpoints
  * breeds list: https://dog.ceo/api/breeds/list/all
  * get single breed image: https://dog.ceo/api/breed/keeshond/images/random
  */
+
+// This function returns (breeds)
+function getDogsBreed() {
+  return fetch(`https://dog.ceo/api/breeds/list/all`)
+    .then((res) => res.json())
+    .then((data) => Object.keys(data.message))
+    .catch((err) => console.log(err));
+}
+
+function renderOptions() {
+  getDogsBreed().then((breeds) => {
+    for (let breed of breeds) {
+      console.log(breed);
+    }
+  });
+}
+
+renderOptions();
+
+// .then works on promise
+// our custom function is not a promise
+// but our custom
